fcf8c2815d825118b1440e5e33d013608b9b8e40
ozkostay <ozkostay@mail.ru>
2023-04-20 15:08:19 +0500

    make redus action 200423

---
+++ src/action/action.js
@@ -0,0 +1,7 @@
+export const CHANGE_NUMBER = 'CHANGE_NUMBER';
+export const SET_USER_VALUE = 'SET_USER_VALUE';
+
+export default {
+  CHANGE_NUMBER,
+  SET_USER_VALUE
+}
---
+++ src/reduser/numberReduser.js
@@ -0,0 +1,25 @@
+import { CHANGE_NUMBER, SET_USER_VALUE } from '../action/action';
+
+const initialState = {
+  value: 0,
+  userValue: 0,
+}
+
+const numberReduser = (state = initialState. action) => {
+  switch (action.type) {
+    case CHANGE_NUMBER:
+      return {
+        ...state,
+        value: state.value + Number(state.userValue)
+      }
+    case SET_USER_VALUE:
+      return {
+        ...state,
+        userValue: action.payload,
+      }
+    default:
+      return state;
+  }
+};
+
+export default numberReduser;
